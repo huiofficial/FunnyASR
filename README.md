ddaac98d2eee8d86c1bda4ed553fa12babc103ab
Hui <tzattack@gmail.com>
2024-04-25 07:16:09 +0800

    update readme and requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # FunnyASR
-A web UI for ASR based on funasr.
+  
+A web UI for ASR based on funasr.  
+
+现在很多手机和电脑端的语音识别 APP 居然要收费，太过分了有木有！那本项目就基于开源的 funasr 进行本地的端到端语音识别，为大家做一个简单的免费语音识别工具。
+
+后续会构建 macOS app，敬请期待！
+
+使用方法：
+
+
+## 支持
+
++ 如果这个项目对你有帮助，请给它一个Star吧！🌟 <a href='https://github.com/huiofficial/FunnyASR'><img src='https://img.shields.io/github/stars/huiofficial/FunnyASR?style=social&label=Star'></a>
++ 支持 FunASR：<a href='https://github.com/alibaba-damo-academy/FunASR.stargazers'><img src='https://img.shields.io/github/stars/alibaba-damo-academy/FunASR.svg?style=social'></a>
+
+如果您有任何问题，欢迎建立 issue 和 pull request，也可以通过我的[邮箱](mailto:tzattack@outlook.com) 与我取得联系。 
+
+如果您觉得我的教程对您有所帮助，且您力所能及的情况下，欢迎扫描下方二维码给我支持  
+
+![付款码](img/pay.png)
+
++ 超过 10 元可以备注私人微信，我会加您。  
++ 超过 1000 元我会邀请您在上海、扬州共进晚餐。  
++ 超过 10000 元我会飞往您所在的城市，与您共进晚餐。
--- run.py
+++ run.py
@@ -2,8 +2,16 @@ from funasr import AutoModel
 from launch import ASRHandler
 import logging
 import librosa
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='ASR Example')
+    parser.add_argument('--input_file', type=str, default='test1.m4a', help='Input audio file')
+    parser.add_argument('--output_text', type=str, default='output.txt', help='Output text file')
+    parser.add_argument('--output_srt', type=str, default='output.srt', help='Output SRT file')
+    parser.add_argument('--output_srt', type=str, default='output.srt', help='Output SRT file')
+    args = parser.parse_args()
+
     funasr_model = AutoModel(model="iic/speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch",
                              model_revision="v2.0.4",
                              vad_model="damo/speech_fsmn_vad_zh-cn-16k-common-pytorch",
